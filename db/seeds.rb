4c79f61a0d673788c9b9a4a37cf1dbbd5652b121
jroyals2 <jamesroyals3@gmail.com>
2017-10-26 16:46:40 -0400

    seeded the databse

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,14 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.fir,st)
+Fridge.destroy_all
+
+trey_fridge = Fridge.create(location:"Kitchen", brand:"GE", size:60, has_food:false, has_drink:true)
+sarah_fridge = Fridge.create(location:"Garage", brand:"LG", size:60, has_food:false, has_drink:true)
+
+Food.create(fridge_id: trey_fridge.id, name: "Cheese", weight: 10, vegan: false, placed_in_fridge: Date.today)
+Food.create(fridge_id: sarah_fridge.id, name: "pizza", weight: 5, vegan: true, placed_in_fridge: Date.today)
+
+Drink.create(fridge_id: trey_fridge.id, name: "beers3", size: 100, is_alcoholic: true)
+Drink.create(fridge_id: sarah_fridge.id, name: "apple juice", size: 50, is_alcoholic: false)
